1b02d422f96368576f7f919a97511da96503200f
MD ABDUR RAHAMAN <abdurrahaman444.spy.com>
2025-03-06 10:22:03 +0600

    sign ing and logout

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
@@ -29,11 +30,50 @@ class AuthController extends Controller
                 'name'=>$request->name,
                 'email'=>$request->email,
                 'password'=>$request->password,
+                // 'password' => bcrypt($request->password),
             ]);
             return response()->json([
                 'status'=>true,
                 'message'=>'User Created Successfully',
                 'user'=>$user,
             ],200 );
+    }
+    public function login(Request $request){
+        $validateUser= Validator::make(
+            $request->all(),
+            [
+                     'email'=> 'required|email',
+                     'password'=>'required',
+                    ]
+            );
+            if($validateUser->fails()){
+                return response()->json([
+                    'status'=>false,
+                    'message'=>'Authentication Faild',
+                    'error'=> $validateUser->errors()->all()
+                ],404);
+            }
+            if(Auth::attempt(['email'=>$request->email, 'password'=>$request->password])){
+            $authUser = Auth::user();
+                return response()->json([
+                    'status'=>true,
+                    'message'=>'User Login Successfully',
+                    'token'=>$authUser->createToken("API Token")->plainTextToken,
+                    'token_type'=>'bearer'
+                ],200 );
+            }else{
+                return response()->json([
+                    'status'=>false,
+                    'message'=>'Email and Passord not matched',
+                ],404);
+            }
+    }
+    public function logout(Request $request){
+        $user = $request->user();
+        $user->tokens()->delete();
+        return response()->json([
+            'status'=>true,
+            'message'=>'You Logout Successfully',
+        ],200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,28 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\PostController;
+use GuzzleHttp\Middleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('signup',[AuthController::class, 'signup']);
+Route::post('login',[AuthController::class, 'login']);
+// simple
+// Route::post('logout',[AuthController::class, 'logout']);
+
+// check login or not
+// with out group
+// Route::post('logout',[AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+// api resource
+// with out group
+// Route::apiResource('posts',PostController::class)->middleware('auth:sanctum');
+
+
+// group api Middleware route
+Route::middleware('auth:sanctum')->group(function(){
+    Route::post('logout',[AuthController::class, 'logout']);
+    Route::apiResource('posts',PostController::class);
+
+});
